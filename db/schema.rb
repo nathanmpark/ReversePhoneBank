56f5224aa1c6a01d0c6490cd99c5fd98d621b356
BootCoder <bootcoder@gmail.com>
2017-01-08 17:48:08 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212045027) do
+ActiveRecord::Schema.define(version: 20161212052000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161212045027) do
     t.datetime "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "owner_id"
   end
 
   create_table "district_addresses", force: :cascade do |t|
