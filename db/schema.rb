fc082ed8729eae9717a5a51b41537e06372e22ae
BootCoder <bootcoder@gmail.com>
2017-02-20 13:59:12 -0800

    add password_digest to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220201223) do
+ActiveRecord::Schema.define(version: 20170220215650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,8 +131,9 @@ ActiveRecord::Schema.define(version: 20170220201223) do
     t.string   "phone"
     t.integer  "address_id"
     t.integer  "user_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.text     "password_digest"
     t.index ["address_id"], name: "index_users_on_address_id", using: :btree
   end
 
