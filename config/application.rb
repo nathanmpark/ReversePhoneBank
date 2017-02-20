4dad5397d9f94a93e3e1c325d7896b22717ce7e6
BootCoder <bootcoder@gmail.com>
2017-02-20 13:39:27 -0800

    add JsonWebToken singleton

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ Bundler.require(*Rails.groups)
 
 module ReversePhoneBank
   class Application < Rails::Application
+
+    # Autoload lib files for JWT
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
