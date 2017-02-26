a86f6351eff5dff0ca2dd841fc4d916c1cf92c47
BootCoder <bootcoder@gmail.com>
2017-02-26 13:55:05 -0800

    resolve bug in Hash creation

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,14 +1,14 @@
 class JsonWebToken
   class << self
 
-    def encode(paylod, exp = 24.hours.from_now)
+    def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
     def decode(token)
       body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-      HashWithIndifferntAccess.new body
+      HashWithIndifferentAccess.new body
     rescue
       nil
     end
